372c1ea929bd2cbbe77e1a56a0e4050ad707acfc
Christian Ingles <christianarguetaaa@gmail.com>
2022-10-20 20:51:33 -0600

    Add main structure

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className=''>Conexion a la API <strong>Json Placeholder</strong> </h1>
+    </div>
+  );
+}
+
+export default App;
