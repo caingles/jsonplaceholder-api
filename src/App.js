1c0920a91512bdda5b4dd3dd1b07423ebe8e6036
Christian Ingles <christianarguetaaa@gmail.com>
2022-10-20 21:22:30 -0600

    Add component TableUser

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import TableUser from './components/TableUser';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container mx-auto mt-5 text-center">
       <h1 className=''>Conexion a la API <strong>Json Placeholder</strong> </h1>
+      <TableUser></TableUser>
     </div>
   );
 }
